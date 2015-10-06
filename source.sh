6d5f58df70a1d1254fc39513be01290a2dc4739b
lfckop <weizhou@mail.ustc.edu.cn>
2015-10-06 11:07:09 +0800

    add

---
+++ source.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# source this file and it will affect temporary shell
+# Usage: source /home/zhouwei/utils/source.sh
+# created by zhouwei on 2015-10-6
+
+# 
+alias ll="ls -alF"
+alias grep="grep --color=auto"
+alias pg="ps aux | grep -v grep | grep"
+
+# modify the command prompt
+export PS1="\[\033[1;32m\][\[\033[0;32m\]\u@\h:\[\033[1;35m\]\w\[\033[1;32m\]]\[\033[1;31m\] \\$\[\033[0m\] "
+
+# add utils to PATH
+export PATH=$PATH:/home/zhouwei/utils
+
+# git auto-completion
+source /home/zhouwei/utils/.git-completion.bash
+
+exit 0
