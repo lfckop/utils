39df1a73c3d4716572d952cbdde8dd8932d33bc7
lfckop <weizhou@mail.ustc.edu.cn>
2015-12-28 12:29:56 +0800

    add

--- .source.sh
+++ .source.sh
@@ -9,11 +9,11 @@ alias ll="ls -alF"
 alias grep="grep --color=auto"
 alias pg="ps aux | grep -v grep | grep"
 alias ng="netstat -anp | grep"
-alias extip="echo $(curl -s http://whatismyip.akamai.com/)"
+alias extip="curl -s http://whatismyip.akamai.com/"
 
 # modify the command prompt
-#export PS1="\[\033[1;32m\][\[\033[0;32m\]\u@\h:\[\033[1;35m\]\w\[\033[1;32m\]]\[\033[1;31m\] \\$\[\033[0m\] "
-export PS1="\[\033[1;35m\]\w\[\033[1;31m\] \\$\[\033[0m\] "
+export PS1="\[\033[1;32m\][\[\033[0;32m\]\u@\h:\[\033[1;35m\]\w\[\033[1;32m\]]\[\033[1;31m\] \\$\[\033[0m\] "
+#export PS1="\[\033[1;35m\]\w\[\033[1;31m\] \\$\[\033[0m\] "
 
 # add utils to PATH
 export PATH=$PATH:/home/zhouwei/utils
