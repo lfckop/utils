27c8945519867871520932ee97195c09c1ef2d5c
lfckop <weizhou@mail.ustc.edu.cn>
2016-01-30 23:06:40 +0800

    add

--- .source.sh
+++ .source.sh
@@ -10,6 +10,7 @@ alias grep="grep --color=auto"
 alias pg="ps aux | grep -v grep | grep"
 alias ng="netstat -anp | grep"
 alias extip="curl -s http://whatismyip.akamai.com/"
+alias e="exit"
 
 # modify the command prompt
 export PS1="\[\033[1;32m\][\[\033[0;32m\]\u@\h:\[\033[1;35m\]\w\[\033[1;32m\]]\[\033[1;31m\] \\$\[\033[0m\] "
