40e1e079ec18bd11f70181f3e4674fd1ffe9c8c9
lfckop <weizhou@mail.ustc.edu.cn>
2016-05-20 18:18:15 +0800

    update .source.sh

--- .source.sh
+++ .source.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # source this file and it only affect the current shell
 # Usage: source /home/zhouwei/utils/.source.sh
-# Or, if you like, you can append 'source /home/zhouwei/utils/.source.sh' to /etc/profile
+# Or, if you like, you can append 'source /home/zhouwei/utils/.source.sh' to /etc/profile, and it'll affect all shells.
 # used when remote login servers through ssh
 # created by zhouwei on 2015-10-6
 
