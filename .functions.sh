58a1903e969b2de2cb1b4aec416c84d9013e8244
lfckop <weizhou@mail.ustc.edu.cn>
2018-08-08 23:34:10 +0800

    fix comments

--- .functions.sh
+++ .functions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # common useful shell functions
-# function name usually started with "f_"
+# function name usually starts with "f_"
 # usage: source this file before you use it
 # created by zhouwei on 2017-12-07
 
-# print error message with red color
+# print error message in red color
 f_error () {
     echo -e "\033[1;31m[ERROR] $@\033[0m"
 }
