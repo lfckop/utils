550cd362b23ad8096dc61429d84948382984e0fc
lfckop <weizhou@mail.ustc.edu.cn>
2018-11-09 21:26:25 +0800

    add

--- .functions.sh
+++ .functions.sh
@@ -4,7 +4,7 @@
 # usage: source this file before you use it
 # created by zhouwei on 2017-12-07
 
-# print error message in red color
+# print error message to stderr in red color
 f_error () {
-    echo -e "\033[1;31m[ERROR] $@\033[0m"
+    echo -e "\033[1;31m[ERROR] $@\033[0m" 1>&2
 }
