fbd90ddda84feea0fe77acdeb758879d41a86734
lfckop <weizhou@mail.ustc.edu.cn>
2018-11-23 11:50:37 +0800

    add

--- .functions.sh
+++ .functions.sh
@@ -5,6 +5,7 @@
 # created by zhouwei on 2017-12-07
 
 # print error message to stderr in red color, and return with status code 1
+# usage: f_error 'error description message'
 f_error () {
     echo -e "\033[1;31m[ERROR] $@\033[0m" 1>&2
     exit 1
