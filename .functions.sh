86600f8d4150ea4d2f3ba12b3d13b293d1efcdac
lfckop <weizhou@mail.ustc.edu.cn>
2019-12-04 14:57:15 +0800

    add

--- .functions.sh
+++ .functions.sh
@@ -4,9 +4,17 @@
 # usage: source this file before you use it
 # created by zhouwei on 2017-12-07
 
-# print error message to stderr in red color, and return with status code 1
+
+# print error message to stderr in red color, and exit with status code 1
 # usage: f_error 'error description message'
 f_error () {
     echo -e "\033[1;31m[ERROR] $@\033[0m" 1>&2
     exit 1
+}
+
+
+# print error message to stderr in red color
+# usage: f_echo_error 'error description message'
+f_echo_error () {
+    echo -e "\033[1;31m[ERROR] $@\033[0m" 1>&2
 }
